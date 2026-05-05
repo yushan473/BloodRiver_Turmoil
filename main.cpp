e69a35246851540999471ee0c78dc8ea2c01ac36
yushan <yushanzhang473@gmail.com>
2026-05-05 14:23:57 +0800

    v0.4

--- main.cpp
+++ main.cpp
@@ -2,17 +2,10 @@
 
 #include <QApplication>
 #include <QDebug>
-#include <QFont>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    // 设置全局默认字体（支持中文的像素风格字体）
-    QFont defaultFont("SimHei", 12);
-    defaultFont.setStyleStrategy(QFont::NoAntialias);
-    a.setFont(defaultFont);
-
     GameWidget w;
     w.show();
     return a.exec();
